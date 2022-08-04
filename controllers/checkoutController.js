cb01adcd4641603cb5ff1c7993ebf433f2944023
Adilson Santos <adilson.fernando@gmail.com>
2022-08-04 13:47:19 -0300

    atualizar as paginas do checkout

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,22 +1,60 @@
+const fs = require("fs");
+const usersJson = require('../users.json')
+const bcrypt = require('bcrypt')
+
 const controllerCheckout = {
     CHECKOUT1: (req, res) =>{
-        res.render('../views/Checkout/checkout1');
+        res.render('./Checkout/checkout1');
     },
     CHECKOUT2: (req, res) =>{
-        res.render('../views/Checkout/checkout2');
+        res.render('./Checkout/checkout2');
+    },
+    CHECKOUT2TIPOFRETE: (req, res) =>{
+        //fazer o redirect
+        //let venda = req.body;
+        res.redirect('../views/checkoutres2')
+    },
+    FormaPagamento: (req, res) =>{
+        //fazer o redirect
+        //let venda = req.body;
+        res.redirect('./Checkout/checkoutres2')
+    },
+    PreviewFinalVenda: (req, res) =>{
+        //fazer o redirect
+        //let venda = req.body;
+        res.redirect('./Checkout/checkoutresPrevVenda')
     },
     CHECKOUT3: (req, res) =>{
-        res.render('../views/Checkout/checkout3');
+        res.render('./Checkout/checkout3');
     },
     CHECKOUT4: (req, res) =>{
-        res.render('../views/Checkout/checkout4');
+        res.render('./Checkout/checkout4');
     },
     CHECKOUTRES: (req, res) =>{
-        res.render('../views/Checkout/checkoutRes');
+        res.render('./Checkout/checkoutRes');
+    },
+    CHECKOUTRES2: (req, res) =>{
+        res.render('./Checkout/checkoutRes2');
+    },
+    CHECKOUTRESPREVVENDA: (req, res) =>{
+        res.render('./Checkout/checkoutResPrevVenda');
+    },
+    SUCESSOCONTROLER: (req, res) =>{
+        res.render('./Checkout/sucessoCheckout');
     },
     SALVARVENDA: (req, res) =>{
         //fazer o redirect
-    }
+        //let venda = req.body;
+        console.log('teste venda');
+        const index = (req, res) => {
+            //Escreva seu código aqui
+            res.cookie('compraprato', 'teste', {maxAge: 100000} )
+        }
+        console.log(req.cookies.compraprato);
+        res.redirect('/confirmarcompra')
+    },
+ 
+
 }
 
 module.exports = controllerCheckout;
--- routes/checkout.js
+++ routes/checkout.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const controllerCheckout = require('../controllers/checkoutController');
+const {check: checar} = require('express-validator'); // importar validator
+let validarVEnda =[
+    // cirar a validação para a venda
+]; //array de validações
 
 router.get('/checkout1', controllerCheckout.CHECKOUT1);
 router.get('/checkout2', controllerCheckout.CHECKOUT2);
+router.post('/checkout2', controllerCheckout.FormaPagamento);
 router.get('/checkout3', controllerCheckout.CHECKOUT3);
 router.get('/checkout4', controllerCheckout.CHECKOUT4);
 router.get('/checkoutRes', controllerCheckout.CHECKOUTRES);
+router.get('/checkoutRes2', controllerCheckout.CHECKOUTRES2);
+router.get('/checkoutResPrevVenda', controllerCheckout.CHECKOUTRESPREVVENDA);
 
+
+router.post('/checkout4', controllerCheckout.PreviewFinalVenda);
+
+router.get('/confirmarcompra', controllerCheckout.SUCESSOCONTROLER)
+router.post('/confirmarcompra', validarVEnda, controllerCheckout.SALVARVENDA)
 
 module.exports = router;
